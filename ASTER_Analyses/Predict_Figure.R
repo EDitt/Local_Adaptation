d486b0f6b53d2f8bce76e0e012fd882e6b585b36
Emily Dittmar <Dittmare@gmail.com>
2019-12-09 15:50:07 -0500

    Update ASTER predict models + code

--- ASTER_Analyses/Predict_Figure.R
+++ ASTER_Analyses/Predict_Figure.R
@@ -3,8 +3,15 @@
 library(dplyr)
 library(aster)
 
+### Load redata.RData
+load("DataFiles/redata.RData")
+
+##############################################
+####### MODEL TESTING FOR PREDICT ############
+##############################################
+
 ### Largest Model without 3-way interaction
-aout.full <- aster(resp ~ varb +
+aout1 <- aster(resp ~ varb +
                      varb:(Population + Year + SoilType + 
                             Population:SoilType + 
                             Population:Year + 
@@ -19,78 +26,72 @@ aout.popsoil <- aster(resp ~ varb + fit:Population:SoilType +
                              Year:SoilType + 
                              Edge),
                    pred, fam, varb, id, root, data = redata)
-anova(aout.popsoil, aout.full) #p<0.0001
+anova(aout.popsoil, aout1) #p<0.0001
 
+#Year x Soil with 'fit'
 aout.yearsoil <- aster(resp ~ varb + fit:Year:SoilType + 
                      varb:(Population + Year + SoilType + 
                              Population:SoilType + 
                              Population:Year + 
                              Edge),
                    pred, fam, varb, id, root, data = redata)
-anova(aout.yearsoil, aout.full) #p<0.0001
+anova(aout.yearsoil, aout1) #p<0.0001
 
+#Pop x Year with 'fit'
 aout.popyear <- aster(resp ~ varb + fit:Population:Year + 
                      varb:(Population + Year + SoilType + 
                              Population:SoilType + 
                              Year:SoilType + 
                              Edge),
                    pred, fam, varb, id, root, data = redata)
-anova(aout.popyear, aout.full) #p<0.0001
+anova(aout.popyear, aout1) #p<0.0001
 
-aout.SoilType <- aster(resp ~ varb + fit:SoilType +
-                     varb:(Population + Year + 
-                             Population:SoilType + 
-                             Population:Year + 
-                             Year:SoilType + 
-                             Edge),
-                   pred, fam, varb, id, root, data = redata)
-anova(aout.SoilType, aout.full) #ns
+### A model that allows all interactions to vary with varb (different slopes for relationship between each variable and fitness component) fits the best
+### Will use this model "aout1" for predict function
 
-aout.Population <- aster(resp ~ varb + fit:Population +
-                         varb:(SoilType + Year + 
-                                 Population:SoilType + 
-                                 Population:Year + 
-                                 Year:SoilType + 
-                                 Edge),
-                       pred, fam, varb, id, root, data = redata)
-anova(aout.Population, aout.full) #ns
+##############################################
+############## ASTER PREDICT #################
+##############################################
 
-aout.Year <- aster(resp ~ varb + fit:Year + 
-                           varb:(SoilType + Population +
-                                   Population:SoilType + 
-                                   Population:Year + 
-                                   Year:SoilType + 
-                                   Edge),
-                         pred, fam, varb, id, root, data = redata)
-anova(aout.Year, aout.full)
+### Predict number of fruits for each population x soil type in each year
+### Will use model that includes all pairwise interactions, but not 3-way interaction (can't compute SEs for this model and interaction not significant)
+### Model allows for a different slope between covariate and each fitness component
 
-aout.Edge <- aster(resp ~ varb + fit:Edge + 
-                     varb:(SoilType + Population + Year +
-                             Population:SoilType + 
-                             Population:Year + 
-                             Year:SoilType),
-                   pred, fam, varb, id, root, data = redata)
-anova(aout.Edge, aout.full) #ns
+### Dataframe for predictions
+newdata <- expand.grid(Population=levels(redata$Population), SoilType=levels(redata$SoilType), Year=levels(redata$Year))
 
-#should I include main effects interaction with fit?
-Mod1 <- aster(resp ~ varb +
-                   varb:(Population + Year + SoilType + 
-                           Population:SoilType + 
-                           Population:Year + 
-                           Year:SoilType + 
-                           Edge),
-                 pred, fam, varb, id, root, data = redata)
+### "Typical Individuals"
+newdata$Edge <- "Non-edge"
 
-Mod2 <- aster(resp ~ varb + fit:(Population + Year + SoilType + Edge) +
-                varb:(Population:SoilType + 
-                        Population:Year + 
-                        Year:SoilType),
-              pred, fam, varb, id, root, data = redata)
+### Add arbitrary values for response vector
+for (v in vars)
+  newdata[[v]] <- 1
 
-anova(Mod2, Mod1) #ns
+newdata$root <- 1 #add the root
 
-pout <- predict(Mod2, newdata = renewdata, varvar = varb, idvar = id, root = root, se.fit = TRUE, amat = amat)
-pout <- predict(aout.full, newdata = renewdata, varvar = varb, idvar = id, root = root, se.fit = TRUE, amat = amat)
+#reshape this dataset
+renewdata <- reshape(newdata, varying = list(vars), direction = "long", timevar = "varb", times = as.factor(vars), v.names = "resp")
+
+## Fitness variable to predict: total number of fruits
+fit <- grepl("Num_frts", as.character(renewdata$varb))
+fit <- as.numeric(fit)
+renewdata$fit <- fit
+
+#check
+with(renewdata, sort(unique(as.character(varb)[fit == 0])))
+with(renewdata, sort(unique(as.character(varb)[fit == 1])))
+
+nind <-nrow (newdata) #num rows of df
+nnode <- length (vars) #3 variables
+amat <-array(0, c(nind, nnode, nind)) #this makes an empty array
+dim(amat)
+
+#set the components to be one for variable of interest (number of fruits) and zero for the others
+for (i in 1:nind)
+  amat[i, grep("Num_frts", vars), i] <- 1
+
+#predicted values
+pout <- predict(aout1, newdata = renewdata, varvar = varb, idvar = id, root = root, se.fit = TRUE, amat = amat)
 
 summary(pout) 
 bar <-cbind(pout$fit, pout$se.fit)
@@ -100,10 +101,12 @@ print(bar)
 ASTER_all_predict <- as.data.frame(bar)
 
 
-####Plot
+##############################################
+################## FIGURE ####################
+##############################################
 
-setEPS
-postscript("TotFit_EachYr_Parents_resp.eps", height=3, width=10)
+#setEPS
+#postscript("TotFit_EachYr_Parents_resp.eps", height=3, width=10)
 
 par(mfrow=c(1,4))
 
@@ -173,5 +176,33 @@ arrows(c(1,1), c(ASTER_all_predict[13:14,1])-c(ASTER_all_predict[13:14,2]), c(1,
 
 arrows(c(2,2), c(ASTER_all_predict[15:16,1])-c(ASTER_all_predict[15:16,2]), c(2,2), c(ASTER_all_predict[15:16,1])+c(ASTER_all_predict[15:16,2]), angle=90, length=0.033, code=3, lwd=1, col=c("black", "orchid1"))
 
-dev.off()
+#dev.off()
+
+##############################################
+########## SIG PAIRWISE DIFFERENCES ##########
+##############################################
+
+## Posthoc significance tests 
+## Difference in predicted number of fruits between source populations on each soil type
+## Bonferroni corrected p-values
+
+## T-test Statistics
+SampleSize <- aggregate(fieldnoNA$PositionColumn, by=list(fieldnoNA$Population, fieldnoNA$SoilType, fieldnoNA$Year), length)
+Df.Stats <- cbind(SampleSize, bar)
+colnames(Df.Stats) <- c("Pop", "Soil", "Year", "N", "Predict", "SE")
+
+Df.Stats$SD <- Df.Stats$SE * sqrt(Df.Stats$N) #standard deviation
+Df.Stats$Var <- Df.Stats$SD^2
+Df.Stats$VarxN <- Df.Stats$Var * Df.Stats$N
+
+#reshape for t-test
+Df.Stats_long <- reshape(Df.Stats, idvar = c("Soil", "Year"), timevar = "Pop", direction = "wide")
+
+#calculating T-statistics
+Df.Stats_long$Pop_diff <- Df.Stats_long$Predict.SerpPop - Df.Stats_long$Predict.SandPop
+Df.Stats_long$CommonVar <- (Df.Stats_long$VarxN.SerpPop + Df.Stats_long$VarxN.SandPop) / (Df.Stats_long$N.SerpPop + Df.Stats_long$N.SandPop - 2)
+Df.Stats_long$Tstat_denominator <- sqrt((Df.Stats_long$CommonVar/Df.Stats_long$N.SerpPop) + (Df.Stats_long$CommonVar/Df.Stats_long$N.SandPop))
+Df.Stats_long$Tvalue <- Df.Stats_long$Pop_diff / Df.Stats_long$Tstat_denominator
+Df.Stats_long$df <- Df.Stats_long$N.SerpPop + Df.Stats_long$N.SandPop - 2
+
 
